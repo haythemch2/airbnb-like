450ca4f7bea01acb90d57dcc19dc366f095e8d5c
haythemch2 <haythem.chibani@gmail.com>
2021-09-23 14:04:23 +0100

    UI init

---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="HomeContainer">
+      <div className="HomeNavBar">
+        <h3>Dary</h3>
+        <h5>login</h5>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
